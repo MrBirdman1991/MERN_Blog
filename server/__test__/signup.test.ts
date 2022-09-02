76b419dfa5c9df28da3c54d90dc3454fd5a9b8cf
julian vogel <birdman-coding@outlook.de>
2022-09-02 22:34:07 +0200

    tests

--- server/__test__/signup.test.ts
+++ server/__test__/signup.test.ts
@@ -21,7 +21,7 @@ describe("given the user input exist", () => {
       password: "4forGlesa!",
     },
     clientIp: "::ffff:127.0.0.1",
-    get: () => "PostmanRuntime/7.29.2",
+    get: (x: string) => "PostmanRuntime/7.29.2",
   };
 
   const userId = new mongoose.Types.ObjectId().toString();
@@ -81,7 +81,7 @@ describe("given the user input exist", () => {
     expect(statusCode).toBe(500);
   });
 
-  it("should call createUser once if user not exists", async () => {
+  it("should call createUser once with user param if user not exists", async () => {
     // @ts-ignore
     UserService.findUser.mockImplementationOnce(() => Promise.resolve(false));
 
@@ -94,5 +94,71 @@ describe("given the user input exist", () => {
     await signUpHandler(req, res, next);
 
     expect(createUserServiceMock).toBeCalledTimes(1);
+    expect(createUserServiceMock).toHaveBeenCalledWith({
+      email: req.body.email,
+      password: req.body.password,
+      userAgent: req.get("UserAgent"),
+      clientIp: req.clientIp,
+    });
   });
+
+  it("should throw 500 status if createUser throws", async () => {
+    // @ts-ignore
+    UserService.findUser.mockImplementationOnce(() => Promise.resolve(false));
+    vi.spyOn(UserService, "createUser").mockRejectedValueOnce("unknown Error");
+
+    const { statusCode } = await request(app)
+      .post(ROUTE_SIGNUP)
+      .send({ ...req.body });
+
+    expect(statusCode).toBe(500);
+  });
+
+  it("should return userPayload with 201 status if user not exists in db", async () => {
+    // @ts-ignore
+    UserService.findUser.mockImplementationOnce(() => Promise.resolve(false));
+
+    vi.spyOn(UserService, "createUser")
+      // @ts-ignore
+      .mockResolvedValueOnce(Promise.resolve({ ...userPayload }));
+
+    const { statusCode, body } = await request(app)
+      .post(ROUTE_SIGNUP)
+      .send({ ...req.body });
+
+    expect(statusCode).toBe(201);
+    expect(body).toEqual(userPayload);
+  });
+});
+
+describe("given the user input doesn't exist", () => {
+  describe.each([
+    ["email", "", "not valid email"],
+    ["email", "stanweb.de", "not valid email"],
+    ["password", "", "Password too short - should be 6 chars"],
+    ["password", "123", "Password too short - should be 6 chars"],
+    ["password", "a".repeat(50), "Password can only be 35 chars long"],
+    ["password", "adddddd", "Password requires one uppercase char"],
+    ["password", "addHdddd", "Password requires one number"],
+    ["password", "addHdddd3", "Password requires one spezial char"],
+  ])(
+    "check validation input %s",
+    (field: string, value: string, expectedMessage: string) => {
+      it(`should return "${expectedMessage}" if value of "${field}" is wrong`, async () => {
+        const user = {
+          email: "stan@web.de",
+          password: "1234567",
+        };
+        // @ts-ignore
+        user[field] = value;
+
+        const { statusCode, body } = await request(app)
+          .post(ROUTE_SIGNUP)
+          .send(user);
+        const singleError = body.filter((item: any) => item.path[1] === field);
+        expect(singleError[0].message).toBe(expectedMessage);
+        expect(statusCode).toBe(422);
+      });
+    }
+  );
 });
